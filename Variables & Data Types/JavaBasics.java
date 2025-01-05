c1241809c3f1772a7f35053719185ca679dd40c6
Davender Singh <davender-singh1@myseneca.ca>
2025-01-04 22:58:04 -0600

    Input in java

--- Variables & Data Types/JavaBasics.java
+++ Variables & Data Types/JavaBasics.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class JavaBasics {
     public static void main(String args[]){
-        System.out.println("****");
+
+        /*System.out.println("****");
         System.out.println("***");
         System.out.println("**");
         System.out.println("*");
@@ -14,9 +17,21 @@ public class JavaBasics {
         double price = 10.5;
         System.out.println(price);
         int number = 25;
-        System.out.println(number);
+        System.out.println(number);*/
         //long
         //double
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
 
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+        
     }
 }
