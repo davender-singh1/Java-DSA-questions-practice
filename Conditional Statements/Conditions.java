103e31bbd51eaf8586d55094bfb80592f4104e70
Davender Singh <davender-singh1@myseneca.ca>
2025-01-15 00:33:36 -0600

    Income Tax Calculator

--- Conditional Statements/Conditions.java
+++ Conditional Statements/Conditions.java
@@ -25,14 +25,34 @@ public class Conditions {
 
 
         // Print if a number is Odd or Even
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
 
-        if(number % 2 == 0){
-            System.out.println("EVEN");
-        } else {
-            System.out.println("ODD");
-        }
+        // if(number % 2 == 0){
+        //     System.out.println("EVEN");
+        // } else {
+        //     System.out.println("ODD");
+        // }
+
+
+
+        // Income tax Calculator
+
+        // Scanner sc = new Scanner(System.in);
+        // int income = sc.nextInt();
+        // int tax;
+
+        // if(income < 500000) {
+        //     tax = 0;
+        // }
+        // else if(income >= 500000 && income < 1000000) {
+        //     tax = (int) (income * 0.2);
+        // }
+        // else {
+        //     tax = (int) (income * 0.3);
+        // }
+
+        // System.out.println("Your tax is :" + tax);
 
     }
 }
