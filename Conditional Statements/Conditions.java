4479d27d2ffd956af46b3e39f8d557daccc5a139
Davender Singh <davender-singh1@myseneca.ca>
2025-01-14 23:01:33 -0600

    largest of 2 num

--- Conditional Statements/Conditions.java
+++ Conditional Statements/Conditions.java
@@ -1,15 +1,24 @@
 public class Conditions {
     public static void main(String args[]){
-        int age = 15;
-        if(age >= 18) {
-            System.out.println("adult : drive, vote");
-        }
+        // int age = 15;
+        // if(age >= 18) {
+        //     System.out.println("adult : drive, vote");
+        // }
 
-        if(age > 13 && age < 18) {
-            System.out.println("teenager");
-        }
-        else {
-            System.out.println("not adult");
+        // if(age > 13 && age < 18) {
+        //     System.out.println("teenager");
+        // }
+        // else {
+        //     System.out.println("not adult");
+        // }
+
+        // Print the largest of 2
+        int A = 1;
+        int B = 5;
+        if (A>=B) {
+            System.out.println("A is largest of 2");   
+        } else {
+            System.out.println("B is largest of 2");
         }
 
     }
